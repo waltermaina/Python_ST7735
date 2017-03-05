7fb910d9596ec928f1e865251719a7c5e9b0a1cb
Clement Skau <cskau+github@gmail.com>
2017-03-05 15:42:13 +0900

    Fixes image.py.

--- examples/image.py
+++ examples/image.py
@@ -38,7 +38,7 @@ SPI_DEVICE = 0
 # SPI_DEVICE = 0
 
 # Create TFT LCD display class.
-disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
+disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
 
 # Initialize display.
 disp.begin()
@@ -48,7 +48,7 @@ print('Loading image...')
 image = Image.open('cat.jpg')
 
 # Resize the image and rotate it so it's 240x320 pixels.
-image = image.rotate(90).resize((240, 320))
+image = image.rotate(90).resize((128, 160))
 
 # Draw the image on the display hardware.
 print('Drawing image')
